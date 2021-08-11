543b88482d03b3d0006ddad48a5545dd9c91846d
RDornodon <archelf.hun@gmail.com>
2021-08-11 16:09:30 +0200

    Shortened

---
+++ twt_66_tests.py
@@ -0,0 +1,11 @@
+test = '''5
+22019020
+20002
+2020
+132020
+20
+'''
+
+#for I in[I:=input]*int(I()):K=I();L=len(K);print('2020'in[K[:4-l]+K[L-l:]for l in range(5)]*(3<L))
+#for I in[I:=input]*int(I()):K=I();print('2020'in[K[:l]+K[l:][l-4:l-4and None]for l in range(5)])
+#for I in[I:=input]*int(I()):K=I();print('2020'in[K[:l]+K[l:][:l-5:-1][::-1]for l in range(5)])
